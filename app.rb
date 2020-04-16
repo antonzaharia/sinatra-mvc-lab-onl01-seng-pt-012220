31e344105e5fce700bef090a54c7f88543d177d8
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-04-16 16:00:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    erb :index
+  end
+  
+  post '/' do 
+    @result = PigLatinizer.new.to_pig_latin(params[:input])
+    
+    erb :result
+  end
 end
